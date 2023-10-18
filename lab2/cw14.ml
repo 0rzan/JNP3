9fec26192716190923ed4bfa2e3ff430542a2c69
Michał Orżanowski <m.orzanowsky@gmail.com>
2023-10-18 11:39:10 +0200

    Fix lab2/cw14

--- lab2/cw14.ml
+++ lab2/cw14.ml
@@ -1,8 +1,8 @@
 let rec aplikuj1 l a = match l with
-    | h :: t -> aplikuj1 t (h a)
+    | h :: t -> h (aplikuj1 t a)
     | _ -> a
 ;;
 
 let aplikuj2 l a =
-    List.fold_left (fun x f -> (f x)) a l
+    List.fold_right (fun f x -> (f x)) l a
 ;;
