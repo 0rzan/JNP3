2d1229de951ba0d08d3a687abf47d507fe9497ca
Michał Orżanowski <m.orzanowsky@gmail.com>
2023-12-03 02:23:07 +0100

    Remake BST to a functor and add ArithmeticExpr module

--- lab5/cw3.ml
+++ lab5/cw3.ml
@@ -1,39 +1,23 @@
-type 'a btree =
-  | Empty
-  | Node of 'a * 'a btree * 'a btree
+module type Comparable = sig
+  type t
+  val compare : t -> t -> int
+end
 
-let rec insert x = function
-  | Empty -> Node(x, Empty, Empty)
-  | Node(value, left, right) ->
-    if x < value then Node (value, insert x left, right)
-    else Node (value, left, insert x right)
-
-let rec read_tree tree =
-  let rec traverse acc = function
-  | Empty -> acc
-  | Node (value, left, right) ->
-    let acc_left = traverse acc left in
-    let acc_value = value :: acc_left in
-    traverse acc_value right
-  in List.rev(traverse [] tree)
-
-let build_tree lst =
-  List.fold_left (fun acc x -> insert x acc) Empty lst
-
-let sort_list lst = read_tree(build_tree lst)
-
-module BST =
+module BST(Key : Comparable) =
   struct
-    type 'a btree =
+    type btree =
       | Empty
-      | Node of 'a * 'a btree * 'a btree
+      | Node of Key.t * btree * btree
+      
     let rec insert x = function
       | Empty -> Node(x, Empty, Empty)
       | Node(value, left, right) ->
-        if x < value then Node (value, insert x left, right)
+        if Key.compare x value < 0 then Node (value, insert x left, right)
         else Node (value, left, insert x right)
+
     let build lst =
       List.fold_left (fun acc x -> insert x acc) Empty lst
+
     let rec read tree =
       let rec traverse acc = function
         | Empty -> acc
@@ -42,12 +26,34 @@ module BST =
           let acc_value = value :: acc_left in
           traverse acc_value right
       in List.rev(traverse [] tree)
+    
     let is_bst tree =
       let rec is_ordered min_val max_val = function
         | Empty -> true
         | Node (value, left, right) ->
-          value >= min_val && value <= max_val
-          && is_ordered min_val value left
-          && is_ordered value max_val right
-      in is_ordered min_int max_int tree
+          match (min_val, max_val) with
+          | (Some min_v, Some max_v) ->
+            Key.compare value min_v >= 0 && Key.compare value max_v <= 0
+            && is_ordered (Some min_v) (Some value) left
+            && is_ordered (Some value) (Some max_v) right
+          | (None, Some max_v) ->
+            Key.compare value max_v <= 0
+            && is_ordered None (Some value) left
+            && is_ordered (Some value) (Some max_v) right
+          | (Some min_v, None) ->
+            Key.compare value min_v >= 0
+            && is_ordered (Some min_v) (Some value) left
+            && is_ordered (Some value) None right
+          | (None, None) ->
+            is_ordered None (Some value) left
+            && is_ordered (Some value) None right
+      in is_ordered None None tree
   end
+
+module Int_BST =
+  BST(
+    struct
+      type t = int
+      let compare = Int.compare
+    end
+  )
--- lab6/cw1.ml
+++ lab6/cw1.ml
@@ -1,31 +1,62 @@
-type expr = 
-| Int of int
-| Plus of expr * expr
-| Times of expr * expr
-| Neg of expr
+module ArithmeticExpr =
+  struct
+    module CharMap = Map.Make(Char)
 
-let parse s =
-  let len = String.length s in
-  let stack = Stack.create () in
-  let rec get_digit c = int_of_string (String.make 1 c) in
+    type t = 
+      | Int of int
+      | Plus of t * t
+      | Times of t * t
+      | Neg of t
+      | Var of char
 
-  for i = 0 to len - 1 do
-    match s.[i] with
-    | '+' ->
-      Stack.push (Plus (Stack.pop stack, Stack.pop stack)) stack
-    | '-' ->
-      Stack.push (Neg (Stack.pop stack)) stack
-    | '*' ->
-      Stack.push (Times (Stack.pop stack, Stack.pop stack)) stack
-    | _ ->
-      Stack.push (Int (get_digit s.[i])) stack
-  done;
+    let parse s =
+      let len = String.length s in
+      let stack = Stack.create () in
+      let is_digit c = c >= '0' && c <= '9' in
 
-  Stack.pop stack
+      for i = 0 to len - 1 do
+        match s.[i] with
+        | '+' ->
+          Stack.push (Plus (Stack.pop stack, Stack.pop stack)) stack
+        | '-' ->
+          Stack.push (Neg (Stack.pop stack)) stack
+        | '*' ->
+          Stack.push (Times (Stack.pop stack, Stack.pop stack)) stack
+        | _ as c ->
+          if is_digit c then
+            Stack.push (Int (int_of_string (String.make 1 c))) stack
+          else if c >= 'a' && c <= 'z' then
+            Stack.push (Var c) stack
+      done;
 
-let rec calc d =
-  match d with 
-    | Int n -> n
-    | Plus (e1, e2) -> calc e1 + calc e2
-    | Times (e1, e2) -> calc e1 * calc e2
-    | Neg e -> -1 * calc e
+      Stack.pop stack
+
+    let rec calcf f d = 
+      match d with
+        | Int n -> n
+        | Plus (e1, e2) -> calcf f e1 + calcf f e2
+        | Times (e1, e2) -> calcf f e1 * calcf f e2
+        | Neg e -> -1 * calcf f e
+        | Var c -> f c
+
+    let rec calc m d =
+      match d with 
+        | Int n -> n
+        | Plus (e1, e2) -> calc m e1 + calc m e2
+        | Times (e1, e2) -> calc m e1 * calc m e2
+        | Neg e -> -1 * calc m e
+        | Var c -> CharMap.find c m 
+
+    let compare e1 e2 = 
+      compare (calcf (fun x -> 0) e1) (calcf (fun x -> 0) e2)
+  end
+
+module ExprBST = BST(ArithmeticExpr)
+
+let expr1 = "x3+"
+let expr2 = "123+*-"
+let expr3 = "0123+*+"
+
+let bst = ExprBST.(Empty |> insert (ArithmeticExpr.parse expr1) |> insert (ArithmeticExpr.parse expr2) |> insert (ArithmeticExpr.parse expr3))
+let bst_sorted = ExprBST.read bst
+let bst_sorted_to_int = List.map (ArithmeticExpr.calcf (fun x -> 0)) bst_sorted
